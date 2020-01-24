d1197fca85fae6705fe865df34883f08f62ab845
Jaheed Haynes <jaheedhaynes@pursuit.org>
2020-01-24 10:09:35 -0500

    Initial Commit

---
+++ Pursuit-Core-iOS-PhotoJournal-Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-PhotoJournal-Assignment
+//
+//  Created by Jaheed Haynes on 1/24/20.
+//  Copyright © 2020 Jaheed Haynes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
